583a1da855edb8c4519b4d20854ce267991694f4
jayasudha1989 <100898392+jayasudha1989@users.noreply.github.com>
2022-03-07 21:38:26 +0800

    update transactionRepo

--- src/main/java/com/example/bank/repositories/TransactionRepository.java
+++ src/main/java/com/example/bank/repositories/TransactionRepository.java
@@ -8,6 +8,5 @@ import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    // TODO Limit to recent transactions and implement separate endpoint to view old transactions
-    //List<Transaction> findBySourceAccountIdOrderByInitiationDate(long id);
+    
 }
